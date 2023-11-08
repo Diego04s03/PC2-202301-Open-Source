fbb52e0117e2b84a77b87b472bf7773000c1cf85
DiegoFake <74078542+Diego04s03@users.noreply.github.com>
2023-11-08 15:49:26 -0500

    Delete u20211c009 directory

--- u20211c009/src/main/java/io/travelers/platform/u20211c009/profiles/domain/service/AgencyService.java
+++
@@ -1,7 +0,0 @@
-package io.travelers.platform.u20211c009.profiles.domain.service;
-
-import io.travelers.platform.u20211c009.profiles.domain.model.entity.Agency;
-
-public interface AgencyService {
-    Agency create(Agency agency);
-}
